29badfb0aa2175eaeda09061162eb12ab4d95b3a
alex300011 <53432470+alex300011@users.noreply.github.com>
2021-03-16 23:53:09 +0300

    Версия 0.1
    собираем все по кусочкам
    дорабатываем интерфейс
    собираем логику

--- scr/int_pyqt5.py
+++ scr/int_pyqt5.py
@@ -1,85 +1,47 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import QTableWidgetItem
 import sys
-from main1 import *  # импорт нашего сгенерированного файла
+import main1
+
 import requests
 from bs4 import BeautifulSoup
 
 
-def start():
-    url = 'https://www.stoloto.ru/rapido2/archive'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    quotes1 = soup.find_all('div', class_='elem')
-    quotes = soup.find_all('span', class_='zone')
+
+
+data = [(1,1),(2,2),(3,3)]
+
+
+class mywindow(QtWidgets.QMainWindow, main1.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton_2.clicked.connect(self.start)
+
+
+        self.response = requests.get('https://www.stoloto.ru/rapido2/archive')
+        self.soup = BeautifulSoup(self.response.text, 'lxml')
+        self.quotes1 = self.soup.find_all('div', class_='elem')
+    #quotes = soup.find_all('span', class_='zone')
 
     # print('-->',quotes1)
     # print(type(quotes1))
 
-    paragraphs = []
+        self.paragraphs = []
 
-    for x in quotes1:
-        paragraphs.append([str(x)])
-        #print(paragraphs)
-data = [(1,1),(2,2),(3,3)]
-
-
-class mywindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(mywindow, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        # Меняем текст
-        self.ui.lineEdit.setText("Добро пожаловать на PythonScripts")
-        # указать максимальную длину
-        self.ui.lineEdit_2.setMaxLength(10)
-        # ввод пароля
-        self.ui.lineEdit_3.setEchoMode(QtWidgets.QLineEdit.Password)
-        # только чтение без изменения содержимого.
-        self.ui.lineEdit_4.setReadOnly(True)
-        # меняем цвет вводимого текста
-        self.ui.lineEdit_5.setStyleSheet("color: rgb(28, 43, 255);")
-        # изменение цвета фона QLineEdit
-        self.ui.lineEdit_6.setStyleSheet("background-color: rgb(28, 43, 255);")
-
-        self.ui.tableWidget.setColumnCount(2)
-        self.ui.tableWidget.setRowCount(3)
-        # очистка таблицы при клике на кнопку.
-        self.ui.pushButton.clicked.connect(self.clear)
-        # кнопка парсера
-        self.ui.pushButton_2.clicked.connect(self.buttonClicked1)
-        # Кол-во рядов меняется в зависимости от значений в data.
-        self.ui.tableWidget.setRowCount(len(data))
-
-        # Кол-во столбцов меняется в зависимости от data.
-        self.ui.tableWidget.setColumnCount(len(data[0]))
-
-        # заголовки для столбцов.
-        self.ui.tableWidget.setHorizontalHeaderLabels(('Марка', 'Год выпуска'))
-        # названия рядов.
-        #self.ui.tableWidget.setVerticalHeaderLabels(('900$', '5000$', '13000$'))
-
-        row = 0
-        for tup in data:
-            col = 0
-
-            for item in tup:
-               cellinfo = QTableWidgetItem(item)
-               # Только для чтения
-               cellinfo.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
-               self.ui.tableWidget.setItem(row, col, cellinfo)
-               col += 1
-            row += 1
-        # Сортировка по году выпуска.
-        # 0 - Марка
-        # 1 - Год выпуска
-        self.ui.tableWidget.sortByColumn(1, QtCore.Qt.AscendingOrder)
+        for x in self.quotes1:
+            self.paragraphs.append([str(x)])
+            print(self.paragraphs)
 
     def buttonClicked1(self):
-        self.textEdit.append(start())
+        self.textEdit.append(self.start)
 
     def clear(self):
         self.ui.tableWidget.clear()
+
+    def start(self):
+        self.listWidget.clear()
+        self.listWidget.addItem(x[:25] for x in self.paragraphs)
 
 
 app = QtWidgets.QApplication([])
